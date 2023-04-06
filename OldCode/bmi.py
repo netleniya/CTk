e1cac32b8b7c514f4eb4220cc91dcc696dff3527
Peter Fiee <peterfiee@icloud.com>
2023-04-06 15:27:21 -0400

    Write out complete structure

--- OldCode/bmi.py
+++ OldCode/bmi.py
@@ -12,6 +12,53 @@ class App(ctk.CTk):
         self.geometry("300x400")
         self.resizable(False, False)
 
+        self.label = ctk.CTkLabel(
+            master=self, text="Calculate BMI", font=ctk.CTkFont(size=20, weight="bold")
+        )
+        self.label.pack()
+
+        self.frame = ctk.CTkFrame(master=self)
+        self.frame.pack(fill="both", padx=10, pady=(20, 20))
+
+        self.forms_frame = ctk.CTkFrame(master=self.frame)
+        self.forms_frame.pack(fill="both", padx=10, pady=(10, 5))
+
+        self.weight_label = ctk.CTkLabel(
+            master=self.forms_frame,
+            text="Weight",
+            font=ctk.CTkFont(size=16, weight="bold"),
+        )
+        self.weight_label.grid(row=0, column=0, padx=20, pady=(10, 5))
+
+        self.weight_entry = ctk.CTkEntry(
+            master=self.forms_frame, placeholder_text="weight (kg)"
+        )
+        self.weight_entry.grid(row=0, column=1, columnspan=1, pady=(10, 5))
+
+        self.height_label = ctk.CTkLabel(
+            master=self.forms_frame,
+            text="Height",
+            font=ctk.CTkFont(size=16, weight="bold"),
+        )
+        self.height_label.grid(row=1, column=0, padx=20, pady=(10, 5))
+
+        self.height_entry = ctk.CTkEntry(
+            master=self.forms_frame, placeholder_text="height (cm)"
+        )
+        self.height_entry.grid(row=1, column=1, columnspan=1, pady=(10, 5))
+
+        self.results_frame = ctk.CTkFrame(master=self.frame)
+        self.results_frame.pack(fill="both", padx=10, pady=(5, 10))
+
+        self.calculate_button = ctk.CTkButton(
+            master=self.results_frame,
+            text="Calcluate",
+        )
+        self.calculate_button.pack()
+
+        self.output = ctk.CTkTextbox(master=self.results_frame)
+        self.output.pack(pady=(10, 10))
+
 
 def main() -> None:
     app = App()
